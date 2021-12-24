9aa6c1a496f9771084e24be8247c1c043d3d8c32
jvol jvolizka <jvoljvolizka@gmail.com>
2021-12-24 20:37:41 +0300

    init

---
+++ contracts/sik.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "./FlashLoanReceiverBase.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Run is FlashLoanReceiverBase, Ownable {
+    constructor(ILendingPoolAddressesProvider _provider)
+        FlashLoanReceiverBase(_provider)
+    {}
+
+    function executeOperation(
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata premiums,
+        address initiator,
+        bytes calldata params
+    ) external override returns (bool) {
+        //
+        // This contract now has the funds requested.
+        // Your logic goes here.
+        //
+
+        // At the end of your logic above, this contract owes
+        // the flashloaned amounts + premiums.
+        // Therefore ensure your contract has enough to repay
+        // these amounts.
+
+        // Approve the LendingPool contract allowance to *pull* the owed amount
+        for (uint256 i = 0; i < assets.length; i++) {
+            uint256 amountOwing = amounts[i] + premiums[i];
+            IERC20(assets[i]).approve(address(LENDING_POOL), amountOwing);
+        }
+
+        return true;
+    }
+
+    function _flashloan(address[] memory assets, uint256[] memory amounts)
+        internal
+    {
+        address receiverAddress = address(this);
+
+        address onBehalfOf = address(this);
+        bytes memory params = "";
+        uint16 referralCode = 0;
+
+        uint256[] memory modes = new uint256[](assets.length);
+
+        // 0 = no debt (flash), 1 = stable, 2 = variable
+        for (uint256 i = 0; i < assets.length; i++) {
+            modes[i] = 0;
+        }
+
+        LENDING_POOL.flashLoan(
+            receiverAddress,
+            assets,
+            amounts,
+            modes,
+            onBehalfOf,
+            params,
+            referralCode
+        );
+    }
+
+    /*
+     *  Flash multiple assets
+     */
+    function flashloan(address[] memory assets, uint256[] memory amounts)
+        public
+        onlyOwner
+    {
+        _flashloan(assets, amounts);
+    }
+
+    /*
+     *  Flash loan 1000000000000000000 wei (1 ether) worth of `_asset`
+     */
+    function flashloan(address _asset, uint256 _amount) public onlyOwner {
+        bytes memory data = "";
+
+        address[] memory assets = new address[](1);
+        assets[0] = _asset;
+
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = _amount;
+
+        _flashloan(assets, amounts);
+    }
+
+    function sikis(
+        address _pair1,
+        address _pair2,
+        uint256 _amount
+    ) {}
+}
